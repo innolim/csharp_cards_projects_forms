3166cad0a7ef26204ea046e5cc0c055efe01eff1
innolim <128082075+innolim@users.noreply.github.com>
2023-06-27 20:53:00 -0400

    Add files via upload

---
+++ Cards' project's form/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Cards__project_s_form
+{
+    partial class Cards
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aceSpadesPictureBox = new System.Windows.Forms.PictureBox();
+            this.tenHeartsPictureBox = new System.Windows.Forms.PictureBox();
+            this.kingClubsPictureBox = new System.Windows.Forms.PictureBox();
+            this.cardListBox = new System.Windows.Forms.ListBox();
+            this.showCardButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenHeartsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kingClubsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // aceSpadesPictureBox
+            // 
+            this.aceSpadesPictureBox.Image = global::Cards__project_s_form.Properties.Resources.Ace_Spades;
+            this.aceSpadesPictureBox.Location = new System.Drawing.Point(92, 33);
+            this.aceSpadesPictureBox.Name = "aceSpadesPictureBox";
+            this.aceSpadesPictureBox.Size = new System.Drawing.Size(159, 172);
+            this.aceSpadesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.aceSpadesPictureBox.TabIndex = 0;
+            this.aceSpadesPictureBox.TabStop = false;
+            this.aceSpadesPictureBox.Visible = false;
+            // 
+            // tenHeartsPictureBox
+            // 
+            this.tenHeartsPictureBox.Image = global::Cards__project_s_form.Properties.Resources._10_Hearts;
+            this.tenHeartsPictureBox.Location = new System.Drawing.Point(313, 33);
+            this.tenHeartsPictureBox.Name = "tenHeartsPictureBox";
+            this.tenHeartsPictureBox.Size = new System.Drawing.Size(150, 172);
+            this.tenHeartsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tenHeartsPictureBox.TabIndex = 1;
+            this.tenHeartsPictureBox.TabStop = false;
+            this.tenHeartsPictureBox.Visible = false;
+            // 
+            // kingClubsPictureBox
+            // 
+            this.kingClubsPictureBox.Image = global::Cards__project_s_form.Properties.Resources.King_Clubs;
+            this.kingClubsPictureBox.Location = new System.Drawing.Point(521, 33);
+            this.kingClubsPictureBox.Name = "kingClubsPictureBox";
+            this.kingClubsPictureBox.Size = new System.Drawing.Size(154, 172);
+            this.kingClubsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.kingClubsPictureBox.TabIndex = 2;
+            this.kingClubsPictureBox.TabStop = false;
+            this.kingClubsPictureBox.Visible = false;
+            // 
+            // cardListBox
+            // 
+            this.cardListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardListBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cardListBox.FormattingEnabled = true;
+            this.cardListBox.ItemHeight = 20;
+            this.cardListBox.Items.AddRange(new object[] {
+            "Ace of Spades",
+            "10 of Hearts",
+            "King of Clubs"});
+            this.cardListBox.Location = new System.Drawing.Point(330, 226);
+            this.cardListBox.Name = "cardListBox";
+            this.cardListBox.Size = new System.Drawing.Size(118, 102);
+            this.cardListBox.TabIndex = 3;
+            // 
+            // showCardButton
+            // 
+            this.showCardButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.showCardButton.Location = new System.Drawing.Point(187, 355);
+            this.showCardButton.Name = "showCardButton";
+            this.showCardButton.Size = new System.Drawing.Size(121, 43);
+            this.showCardButton.TabIndex = 4;
+            this.showCardButton.Text = "Show Card";
+            this.showCardButton.UseVisualStyleBackColor = true;
+            this.showCardButton.Click += new System.EventHandler(this.showCardButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.exitButton.Location = new System.Drawing.Point(494, 355);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(114, 43);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Cards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showCardButton);
+            this.Controls.Add(this.cardListBox);
+            this.Controls.Add(this.kingClubsPictureBox);
+            this.Controls.Add(this.tenHeartsPictureBox);
+            this.Controls.Add(this.aceSpadesPictureBox);
+            this.Name = "Cards";
+            this.Text = "Cards";
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenHeartsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kingClubsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox aceSpadesPictureBox;
+        private System.Windows.Forms.PictureBox tenHeartsPictureBox;
+        private System.Windows.Forms.PictureBox kingClubsPictureBox;
+        private System.Windows.Forms.ListBox cardListBox;
+        private System.Windows.Forms.Button showCardButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Cards' project's form/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards__project_s_form
+{
+    public partial class Cards : Form
+    {
+        public Cards()
+        {
+            InitializeComponent();
+        }
+
+        //ShowCard method to accept a string that names selected card, displays that card
+        private void ShowCard(string card) // variable parameter is string named card
+        {
+            switch (card) //  if card is equal to " ", show cards corresponding to that string
+            {
+                case "Ace of Spades":
+                    ShowAceSpades(); //Calling ShowAceSpades method
+                    break;
+                case "10 of Hearts":
+                    ShowTenHearts(); // calling ShowTenHearts method
+                    break;
+                case "King of Clubs":
+                    ShowKingClubs(); // calling ShowKingClubs method
+                    break;
+            }
+        }
+
+        private void ShowAceSpades() // ShowAceSpades method to only show spades card
+        {
+            aceSpadesPictureBox.Visible = true; //spades card is visible
+            tenHeartsPictureBox.Visible = false; // invisible for the others
+            kingClubsPictureBox.Visible = false;
+        }
+
+        private void ShowTenHearts() // ShowTenHearts method to only show heart card
+        {
+            tenHeartsPictureBox.Visible = true; // heart card visible
+            aceSpadesPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = false;
+        }
+
+        private void ShowKingClubs() // ShowKingClubs method to only show king card
+        {
+            kingClubsPictureBox.Visible = true; // king card visible
+            aceSpadesPictureBox.Visible = false;
+            tenHeartsPictureBox.Visible = false;
+        }
+
+        private void showCardButton_Click(object sender, EventArgs e) // event handler method to display selected card in ListBox
+        {
+            if (cardListBox.SelectedIndex != -1)
+            {
+                ShowCard(cardListBox.SelectedItem.ToString()); // calling ShowCard method           
+            }
+            else
+            {
+                MessageBox.Show("Please select a card from the list box."); // if user doesn't choose string from ListBox
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
